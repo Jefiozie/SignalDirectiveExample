cde481ab412f1ed9624a5156412bf88d6d4e774e
Jeffrey Bosch <jeffrey@stiply.nl>
2024-02-02 08:31:29 +0100

    control flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
+    imports: []
 })
 export class AppComponent {
   title = 'dirWithInput';
